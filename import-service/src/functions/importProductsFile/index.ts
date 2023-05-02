40ac846429374e021f017e96e34377649b16fe84
Yuliya_Miatlionak <Yuliya_Miatlionak@epam.com>
2023-05-02 14:16:24 +0300

    add authorization-service func

--- import-service/src/functions/importProductsFile/index.ts
+++ import-service/src/functions/importProductsFile/index.ts
@@ -7,6 +7,13 @@ export default {
       http: {
         method: "get",
         path: "import",
+        cors: true,
+        authorizer: {
+          arn: "arn:aws:lambda:${self:provider.region}:${aws:accountId}:function:authorization-service-dev-basicAuthorizer",
+          type: "request",
+          identitySource: "method.request.header.Authorization",
+          resultTtlInSeconds: 0,
+        },
         request: {
           parameters: {
             querystrings: {
